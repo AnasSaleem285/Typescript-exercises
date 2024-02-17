80c8d80c2db85c5417ed1c15a50e65e1436215bb
ANAS SALEEM <anassaleem285@gmail.com>
2024-02-17 23:43:35 +0500

    Add files via upload

---
+++ Exercise3.js
@@ -0,0 +1,8 @@
+//  Exercise 3
+//""Famous Quote program""
+// Find a quote from a famous person you admire. Print the quote and the name of its author. 
+//Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+var author = ("Prophet Muhammad (Pbuh)");
+var quote = (" Say what is true, although it may be bitter and displeasing to people.");
+console.log("".concat(author, " once said, \"").concat(quote, "\""));
