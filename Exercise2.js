ab166fa254b7f96c6e81f429f25b69ea29eaa84a
ANAS SALEEM <anassaleem285@gmail.com>
2024-02-17 23:06:27 +0500

    Update Exercise2.js

--- Exercise2.js
+++ Exercise2.js
@@ -1,4 +1,4 @@
-// project no.1
+// Exercise2.ts
 //Store a person’s name in a variable, 
 //and then print that person’s name in lowercase, uppercase, and titlecase.
 var personname = "anmol saleem";
